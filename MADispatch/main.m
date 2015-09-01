10f963d2845407f3fcc0c3627ca0644971420439
Mike Ash <mike@mikeash.com>
2015-08-31 22:38:55 -0400

    Initial Commit

---
+++ MADispatch/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MADispatch
+//
+//  Created by Michael Ash on 8/31/15.
+//  Copyright © 2015 mikeash. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
